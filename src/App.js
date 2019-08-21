f5ff8329f7f7199f988305f02d43559c6674b3f4
sameera <talangama@gmail.com>
2019-08-21 12:20:33 +0530

    Add youtube packages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import YTSearch from 'youtube-api-search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = 'AIzaSyD5nWB3Kp_iUcuSA4bVi563FR1OgGRtIyQ';
+
+class App  extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {videos : []};
+
+        YTSearch({
+            key: API_KEY,
+            term: 'surfboard'
+        }, function (data) {
+            this.setState({videos:data});
+        });
+    }
+
+    render() {
+        
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
